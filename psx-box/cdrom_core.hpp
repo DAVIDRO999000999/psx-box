56c27dc8981932c37442ef209602760fbbb41a19
wiired24 <vanitas131@gmail.com>
2017-04-24 05:44:31 -0500

    Update cdrom_core.hpp

--- psx-box/cdrom_core.hpp
+++ psx-box/cdrom_core.hpp
@@ -7,24 +7,31 @@
 //Create a new namespace
 namespace cdrom {
   struct state_t {
-  // All the states that the CDROM can be in
+  
+    // All the states that the CDROM can be in
     uint32_t interrupt_enable;
     uint32_t interrupt_disable;
     uint32_t index;
     uint32_t command;
     bool has_command;
-    //I dont even know what to comment here?
+    
+    
     std::deque<uint8_t> args_fifo;
     std::deque<uint8_t> resp_fifo;
     std::deque<uint8_t> data_fifo;
  };
 
   extern state_t state;
-
+  
+  // bus_read function reads the address passed to it
   uint32_t bus_read(int width, uint32_t address);
-
+  
+  
+  // bus_write function writes data at the address passed to it
   void bus_write(int width, uint32_t address, uint32_t data);
 
+  
+  // Function to run the CD
   void run();
 
 }
