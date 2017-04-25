efa0570dc539e076f8a4ab7da5afa05bd71a7d0a
wiired24 <vanitas131@gmail.com>
2017-04-25 02:11:06 -0500

    Update vram.cpp
    Fixed some comments and added in some

--- psx-box/vram.cpp
+++ psx-box/vram.cpp
@@ -1,25 +1,27 @@
 #include <pch.h>
 #include "vram.hpp"
 #include "utility.hpp"
-// im sure this is obvious but were callin the other headers so we can use them here
+// Calling the other headers so we can use them here
 
 static utility::memory_t<20> buffer;
 
 uint16_t* vram::get_pointer() {
       return &buffer.h[0];
 }
-// the above (i think) is calling the vram pointer and multiplying it with the uint16_t value, then buffers it. (maybe?)
+// get_pointer function gets the address and we return the address of the buffer in memory
 
 uint16_t vram::read(int x, int y) {
 	return buffer.h[(y * 1024) + x];
-	// more of the same, i think
+	// read() reads in data that gets passed to it at x and y and returns 
+	// the modified buffer
 }
 
 void vram::write(int x, int y, uint16_t data) {
       if (x < 0 || x > 0x400) return;
       if (y < 0 || y > 0x200) return;
-// were writing to the vram depending on the data we just called
+// were writing to the vram depending on the data we just passed to it
 
+	
 // Now we just buffer the system
 buffer.h[(y * 1024) + x] = data;
 }
