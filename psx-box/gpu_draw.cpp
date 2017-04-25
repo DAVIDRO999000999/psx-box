0513ea7f9e139cb0d581e09bbfab545667028ee3
wiired24 <vanitas131@gmail.com>
2017-04-25 16:32:13 -0500

    Update gpu_draw.cpp
    Fixed a typo where the v was left off of void xD
    
    Also added in more comments

--- psx-box/gpu_draw.cpp
+++ psx-box/gpu_draw.cpp
@@ -1,37 +1,39 @@
-//Add the required libraries
+#include <pch.h>
 #include "gpu_core.hpp"
 #include "vram.hpp"
 
+
+// Min/Max Value stuff
 template<int min, int max>
 int clip(int value) {
-  if (value <= min) return min;
-  if (value >= max) return max;
-  return value;
+	if (value <= min) return min;
+	if (value >= max) return max;
+	return value;
 };
 
 static int dither_lut[4][4] = {
-  { -4,  0, -3,  1 },
-  {  2, -2,  3, -1 },
-  { -3,  1, -4,  0 },
-  {  3, -1,  2, -2 }
+	{ -4,  0, -3,  1 },
+	{ 2, -2,  3, -1 },
+	{ -3,  1, -4,  0 },
+	{ 3, -1,  2, -2 }
 };
 
 //Init gpu::draw_point function
-oid gpu::draw_point(int x, int y, int r, int g, int b) {
-  if (x < int(state.drawing_area_x1) || x > int(state.drawing_area_x2)) return;
-  if (y < int(state.drawing_area_y1) || y > int(state.drawing_area_y2)) return;
+void gpu::draw_point(int x, int y, int r, int g, int b) {
+	if (x < int(state.drawing_area_x1) || x > int(state.drawing_area_x2)) return;
+	if (y < int(state.drawing_area_y1) || y > int(state.drawing_area_y2)) return;
 
-  auto dither = dither_lut[y & 3][x & 3];
+	auto dither = dither_lut[y & 3][x & 3];
 
-  r = clip<0, 255>(r + dither);
-  g = clip<0, 255>(g + dither);
-  b = clip<0, 255>(b + dither);
+	r = clip<0, 255>(r + dither);
+	g = clip<0, 255>(g + dither);
+	b = clip<0, 255>(b + dither);
 
-  auto color =
-      (((r >> 3) & 0x1f) <<  0) |
-      (((g >> 3) & 0x1f) <<  5) |
-      (((b >> 3) & 0x1f) << 10);
+	auto color =
+		(((r >> 3) & 0x1f) << 0) |
+		(((g >> 3) & 0x1f) << 5) |
+		(((b >> 3) & 0x1f) << 10);
 
-  //Write the color along with the x and y coords into vram
-  vram::write(x, y, uint16_t(color));
+	//Write the color along with the x and y coords into vram
+	vram::write(x, y, uint16_t(color));
 }
