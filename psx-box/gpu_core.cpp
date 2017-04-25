a6d0c7d1a8e8304734f361a2d2e3d1ba082d031f
wiired24 <vanitas131@gmail.com>
2017-04-25 14:33:56 -0500

    Update gpu_core.cpp
    Fixed some errors that we're present as well as added in some comments and improved comments

--- psx-box/gpu_core.cpp
+++ psx-box/gpu_core.cpp
@@ -1,107 +1,113 @@
-//Include critical libs
+
+// Including needed libraries
+#include <pch.h>
 #include <cassert>
 #include "gpu_core.hpp"
 #include "bus.hpp"
 #include "vram.hpp"
 
-//New gpu state;
+// setting up gpu state in the gpu namespace
 gpu::state_t gpu::state;
 
-//New uint32_t with the assigned function of gpu::data
+
+// The data function collects data for the GPU 
 uint32_t gpu::data() {
-  if (gpu::state.gpu_to_cpu_transfer.run.active) {
-    auto lower = vram_transfer();
-    auto upper = vram_transfer();
+	if (gpu::state.gpu_to_cpu_transfer.run.active) {
+		auto lower = vram_transfer();
+		auto upper = vram_transfer();
 
-    return (upper << 16) | lower;
-  }
+		return (upper << 16) | lower;
+	}
 
-  return 0;
+	return 0;
 }
 
-//Another uint32_t with the assigned function of gpu::stat
+// This function also gets data and send it to the GPU
 uint32_t gpu::stat() {
-  //  19    Vertical Resolution         (0=240, 1=480, when Bit22=1)  ;GP1(08h).2
-  //  26    Ready to receive Cmd Word   (0=No, 1=Ready)  ;GP0(...) ;via GP0
-  //  27    Ready to send VRAM to CPU   (0=No, 1=Ready)  ;GP0(C0h) ;via GPUREAD
-  //  28    Ready to receive DMA Block  (0=No, 1=Ready)  ;GP0(...) ;via GP0
+	//  19    Vertical Resolution         (0=240, 1=480, when Bit22=1)  ;GP1(08h).2
+	//  26    Ready to receive Cmd Word   (0=No, 1=Ready)  ;GP0(...) ;via GP0
+	//  27    Ready to send VRAM to CPU   (0=No, 1=Ready)  ;GP0(C0h) ;via GPUREAD
+	//  28    Ready to receive DMA Block  (0=No, 1=Ready)  ;GP0(...) ;via GP0
 
-  return (gpu::state.status & ~0x00080000) | 0x1c002000;
+	return (gpu::state.status & ~0x00080000) | 0x1c002000;
 }
 
-//ANOTHER uint32_t with the assigned function of gpu::bus_read
+
+// This function reads in data from the CPU BUS at the given address
 uint32_t gpu::bus_read(int width, uint32_t address) {
-  assert(width == WORD);
+	assert(width == WORD);
 
-  switch (address) {
-    case 0x1f801810:
-      return data();
+	switch (address) {
+	case 0x1f801810:
+		return data();
 
-    case 0x1f801814:
-      return stat();
+	case 0x1f801814:
+		return stat();
 
-    default:
-      return 0;
-  }
+	default:
+		return 0;
+	}
 }
 
-//New gpu::bus_write function
+
+// THis function writes data on the bus at the given address
 void gpu::bus_write(int width, uint32_t address, uint32_t data) {
-  assert(width == WORD);
+	assert(width == WORD);
 
-  switch (address) {
-    case 0x1f801810:
-      return gp0(data);
+	switch (address) {
+	case 0x1f801810:
+		return gp0(data);
 
-    case 0x1f801814:
-      return gp1(data);
-  }
+	case 0x1f801814:
+		return gp1(data);
+	}
 }
 
-//New uint16_t with the assigned action of gpu::vram_transfer()
+// Pretty self explanatory Transfers vram
 uint16_t gpu::vram_transfer() {
-  auto &transfer = gpu::state.gpu_to_cpu_transfer;
-  if (!transfer.run.active) {
-    return 0;
-  }
+	auto &transfer = gpu::state.gpu_to_cpu_transfer;
+	if (!transfer.run.active) {
+		return 0;
+	}
 
-  auto data = vram::read(transfer.reg.x + transfer.run.x,
-                         transfer.reg.y + transfer.run.y);
 
-  transfer.run.x++;
+	// Assiging the data to be equal to whatever value is returned by the read function()
+	auto data = vram::read(transfer.reg.x + transfer.run.x,
+		transfer.reg.y + transfer.run.y);
 
-  if (transfer.run.x == transfer.reg.w) {
-    transfer.run.x = 0;
-    transfer.run.y++;
+	transfer.run.x++;
 
-    if (transfer.run.y == transfer.reg.h) {
-      transfer.run.y = 0;
-      transfer.run.active = false;
-    }
-  }
+	if (transfer.run.x == transfer.reg.w) {
+		transfer.run.x = 0;
+		transfer.run.y++;
 
-  return data;
+		if (transfer.run.y == transfer.reg.h) {
+			transfer.run.y = 0;
+			transfer.run.active = false;
+		}
+	}
+
+	return data;
 }
 
-//New gpu::vram_transfer function
 void gpu::vram_transfer(uint16_t data) {
-  auto &transfer = gpu::state.cpu_to_gpu_transfer;
-  if (!transfer.run.active) {
-    return;
-  }
+	auto &transfer = gpu::state.cpu_to_gpu_transfer;
+	if (!transfer.run.active) {
+		return;
+	}
 
-  vram::write(transfer.reg.x + transfer.run.x,
-              transfer.reg.y + transfer.run.y, uint16_t(data));
+	vram::write(transfer.reg.x + transfer.run.x,
+		transfer.reg.y + transfer.run.y, uint16_t(data));
 
-  transfer.run.x++;
+	transfer.run.x++;
 
-  if (transfer.run.x == transfer.reg.w) {
-    transfer.run.x = 0;
-    transfer.run.y++;
+	if (transfer.run.x == transfer.reg.w) {
+		transfer.run.x = 0;
+		transfer.run.y++;
 
-    if (transfer.run.y == transfer.reg.h) {
-      transfer.run.y = 0;
-      transfer.run.active = false;
-    }
-  }
+		if (transfer.run.y == transfer.reg.h) {
+			transfer.run.y = 0;
+			transfer.run.active = false;
+		}
+	}
 }
