d64b92248e53cab86f8210ed62dbecf2d2570116
wiired24 <vanitas131@gmail.com>
2017-04-25 02:05:25 -0500

    Update vram.hpp
    Added in some comments for better readability.

--- psx-box/vram.hpp
+++ psx-box/vram.hpp
@@ -3,12 +3,16 @@
 
 #include <cstdint>
 
+// Setting up namespace for virtual ram
 namespace vram {
 
+// This Function gets a pointer  
 uint16_t *get_pointer();
 
+// Reads in data at x and y  
 uint16_t read(int x, int y);
 
+// Writes given data at x and y  
 void write(int x, int y, uint16_t data);
 
 }
