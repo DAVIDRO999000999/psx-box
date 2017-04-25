b4b7d2d254014822d45238bf220b5da48843d749
wiired24 <vanitas131@gmail.com>
2017-04-25 14:24:46 -0500

    Update gpu_core.hpp
    Fixed over 50 Errors that we're present in the original code you gave for approval. Also added in more comments of my own as well.

--- psx-box/gpu_core.hpp
+++ psx-box/gpu_core.hpp
@@ -1,151 +1,162 @@
-//Tell the compiler what we are doing here
 #ifndef PSXBOX_GPU_CORE_HPP
 #define PSXBOX_GPU_CORE_HPP
 
-//Include all the libraries
+// Include all other libraries
 #include <cstdint>
 #include <queue>
 #include "utility.hpp"
 
-//New namespace
+// Create new namespace gpu
 namespace gpu {
-  struct state_t {
-    //Bunch of variables that help us with drawing
-    uint32_t status = 0x14802000;
-    uint32_t texture_window_mask_x;
-    uint32_t texture_window_mask_y;
-    uint32_t texture_window_offset_x;
-    uint32_t texture_window_offset_y;
-    uint32_t drawing_area_x1;
-    uint32_t drawing_area_y1;
-    uint32_t drawing_area_x2;
-    uint32_t drawing_area_y2;
-    uint32_t x_offset;
-    uint32_t y_offset;
-    uint32_t display_area_x;
-    uint32_t display_area_y;
-    uint32_t display_area_x1;
-    uint32_t display_area_y1;
-    uint32_t display_area_x2;
-    uint32_t display_area_y2;
-    bool textured_rectangle_x_flip;
-    bool textured_rectangle_y_flip;
 
-    struct {
-      uint32_t buffer[16];
-      int wr;
-      int rd;
-   } fifo;
-   
-    struct {
-      struct {
-        int x;
-        int y;
-        int w;
-        int h;
-      } reg;
-      
-    struct {
-      bool active;
-      int x;
-      int y;
-    } run;
-  } cpu_to_gpu_transfer;
-  
-  struct {
-    struct {
-      int x;
-      int y;
-      int w;
-      int h;
-    } reg;
-    
-    struct {
-      bool active;
-      int y;
-      int x;
-    } run;
-  } gpu_to_cpu_transfer;
-};
 
-    extern state_t state;
-    
-    //New uint32_t function named bus_read
-    uint32_t bus_read(int width, uint32_t address);
-    
-    //New return type void function with the name bus_write
-    void bus_write(int width, uint32_t address, uint32_t data);
-    
-    //2 New uint32_t vars
-    uint32_t data(); // Data
-    uint32_t stat(); // Status
-    
-    //New functions with the name gp0 and gp1
-    void gp0(uint32_t data);
-    void gp1(uint32_t data);
-    
-    //New vram_transfer function
-    void vram_transfer(uint16_t data);
-    
-    //We call this new function here
-    uint16_t vram_transfer();
-    
-    //Some color and drawing code
-    struct color_t {
-      int r;
-      int g;
-      int b;
-    };
-    
-    struct point_t {
-      int x;
-      int y;
-    };
-    
-    //New draw point function
-    void draw_point(int x, int y, int r, int g, int b);
-    
-    //New namespace :D
-    namespace gouraud {
-      struct pixel_t P
-        point_t point;
-        color_t color;
-      };
-      
-      template<int size>
-      struct polygon_t {
-        pixel_t v[size];
-      };
-      
-      //Some polygon drawing functions with the return type void
-      void draw_poly3(const gpu::gouraud::polygon_t<3> &p);
-      void draw_poly4(const gpu::gouraud::polygon_t<4> &p);
-    }
-    
-    //ANOTHER NAMESPACE :D
-    namespace texture {
-      struct pixel_t {
-      //Useful vars
-        point_t point;
-        color_t color;
-        int u;
-        int v;
-     };
-     
-     template<int size>
-     struct polygon_t {
-      pixel_t v[size];
-      int clut_x;
-      int clut_y;
-      int base_u;
-      int base_v;
-      int depth;
-    };
-    
-    //Functions with the same name as the ones from the last namespace, but different use
-    void draw_poly3(const polygon_t<3> &p);
-    void draw_poly4(const polygon_t<4> &p);
-   } 
+	// Setting up struct to draw to different areas
+	struct state_t {
+		uint32_t status = 0x14802000;
+		uint32_t texture_window_mask_x;
+		uint32_t texture_window_mask_y;
+		uint32_t texture_window_offset_x;
+		uint32_t texture_window_offset_y;
+		uint32_t drawing_area_x1;
+		uint32_t drawing_area_y1;
+		uint32_t drawing_area_x2;
+		uint32_t drawing_area_y2;
+		uint32_t x_offset;
+		uint32_t y_offset;
+		uint32_t display_area_x;
+		uint32_t display_area_y;
+		uint32_t display_area_x1;
+		uint32_t display_area_y1;
+		uint32_t display_area_x2;
+		uint32_t display_area_y2;
+		bool textured_rectangle_x_flip;
+		bool textured_rectangle_y_flip;
+
+		struct {
+			uint32_t buffer[16];
+			int wr;
+			int rd;
+		} fifo;
+
+		struct {
+			struct {
+				int x;
+				int y;
+				int w;
+				int h;
+			} reg;
+
+			struct {
+				bool active;
+				int x;
+				int y;
+			} run;
+		} cpu_to_gpu_transfer;
+
+		struct {
+			struct {
+				int x;
+				int y;
+				int w;
+				int h;
+			} reg;
+
+			struct {
+				bool active;
+				int x;
+				int y;
+			} run;
+		} gpu_to_cpu_transfer;
+	};
+
+	extern state_t state;
+
+
+	// Calling our bus_read function we made in bus.hpp
+	uint32_t bus_read(int width, uint32_t address);
+
+
+	// Calling bus_Write also from bus.hpp
+	void bus_write(int width, uint32_t address, uint32_t data);
+
+
+	uint32_t data();
+
+
+	uint32_t stat();
+
+
+	void gp0(uint32_t data);
+
+
+	void gp1(uint32_t data);
+
+
+	void vram_transfer(uint16_t data);
+
+
+	uint16_t vram_transfer();
+
+
+	// Some color and drawing code
+	struct color_t {
+		int r;
+		int g;
+		int b;
+	};
+
+	struct point_t {
+		int x;
+		int y;
+	};
+
+	// New draw point function
+	void draw_point(int x, int y, int r, int g, int b);
+
+
+	// Creating a new namespace called gouraud
+	namespace gouraud {
+		struct pixel_t {
+			point_t point;
+			color_t color;
+		};
+
+		template<int size>
+		struct polygon_t {
+			pixel_t v[size];
+		};
+
+
+		// Polygon Drawing fucntions
+		void draw_poly3(const gpu::gouraud::polygon_t<3> &p);
+
+		void draw_poly4(const gpu::gouraud::polygon_t<4> &p);
+	}
+
+
+	// Creating a namespace called texture
+	namespace texture {
+		struct pixel_t {
+			point_t point;
+			color_t color;
+			int u;
+			int v;
+		};
+
+		template<int size>
+		struct polygon_t {
+			pixel_t v[size];
+			int clut_x;
+			int clut_y;
+			int base_u;
+			int base_v;
+			int depth;
+		};
+
+		void draw_poly3(const polygon_t<3> &p);
+
+		void draw_poly4(const polygon_t<4> &p);
+	}
 }
 
 #endif //PSXBOX_GPU_CORE_HPP
